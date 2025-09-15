7818e73cbd14ccced5fc78ee03ebbaed0853cacc
Dima <dimarastvorcev6123@gmail.com>
2025-09-15 13:53:50 +0300

    Remove MyLinkedHashMap and move functionality into InMemoryHistoryManager

--- src/history/InMemoryHistoryManager.java
+++ src/history/InMemoryHistoryManager.java
@@ -1,31 +1,96 @@
 package history;
 
 import task.Task;
-import utils.collection.MyLinkedHashMap;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final MyLinkedHashMap<Integer, Task> history;
+    private Node<Task> head;
+    private Node<Task> tail;
+    private final HashMap<Integer, Node<Task>> hashMap;
+
+    class Node<V> {
+        Node<V> next;
+        Node<V> prev;
+        V value;
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            Node<V> node = (Node<V>) o;
+            return Objects.equals(next, node.next) && Objects.equals(prev, node.prev) && Objects.equals(value, node.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(next, prev, value);
+        }
+
+        public Node(V value, Node<V> prev, Node<V> next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
 
     public InMemoryHistoryManager() {
-        history = new MyLinkedHashMap<>();
+        hashMap = new HashMap<>();
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(history.reverseValues());
+        var list = new ArrayList<Task>();
+        var node = tail;
+        while (node != null) {
+            list.add(node.value);
+            node = node.prev;
+        }
+        return list;
+    }
+
+
+    @Override
+    public void add(Task value) {
+        if (hashMap.containsKey(value.getId())) {
+            var oldValue = hashMap.get(value.getId());
+            oldValue.value = value;
+        }
+        var newTail = new Node<>(value, tail, null);
+        if (head == null) {
+            head = newTail;
+            newTail.prev = null;
+        } else if (tail == null) {
+            tail = newTail;
+            newTail.prev = head;
+            head.next = tail;
+        } else {
+            tail.next = newTail;
+            tail = newTail;
+        }
+        hashMap.put(value.getId() , newTail);
     }
 
     @Override
-    public void add(Task task) {
-        remove(task.getId());
-        history.put(task.getId(), task);
+    public void remove(int key) {
+        var i = hashMap.get(key);
+
+        if (i == null) {
+            return;
+        }
+
+        if (i == tail) {
+            tail = i.prev;
+            tail.next = null;
+        } else if (i == head) {
+            head = i.next;
+            if (head != null) {
+                head.prev = null;
+            }
+        } else {
+            i.prev.next = i.next;
+            i.next.prev = i.prev;
+        }
+        hashMap.remove(key);
     }
 
-    @Override
-    public void remove(int id) {
-        history.remove(id);
-    }
 }
--- test/HistoryManagerTest.java
+++ test/HistoryManagerTest.java
@@ -1,8 +1,11 @@
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import task.Task;
 import history.HistoryManager;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,8 +13,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class HistoryManagerTest {
     private static HistoryManager historyManager;
 
-    @BeforeAll
-    static void setup() {
+    @BeforeEach
+    void setup() {
         historyManager = Managers.getDefaultHistory();
     }
 
@@ -26,5 +29,42 @@ public class HistoryManagerTest {
         assertEquals("Task 12", history.get(0).getName());
         assertEquals("Task 1", history.get(11).getName());
 
+    }
+
+    @Test
+    void shouldRemoveElementCorrectly() {
+        Task task = new Task(1, "Test Task", "Description");
+        historyManager.add(task);
+
+        historyManager.remove(1);
+
+        Assertions.assertTrue(historyManager.getHistory().isEmpty());
+    }
+
+    @Test
+    void shouldReturnNullWhenRemovingNonExistentKey() {
+        historyManager.remove(1);
+
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void shouldMaintainOrderAfterRemoval() {
+        Task task1 = new Task(1, "First", "Description 1");
+        Task task2 = new Task(2, "Second", "Description 2");
+        Task task3 = new Task(3, "Third", "Description 3");
+
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+
+        historyManager.remove(2);
+
+        Collection<Task> values = historyManager.getHistory();
+        List<Task> valueList = new ArrayList<>(values);
+
+        Assertions.assertEquals(2, valueList.size());
+        Assertions.assertEquals(task1, valueList.get(1));
+        Assertions.assertEquals(task3, valueList.get(0));
     }
 }
